a3fc16ca79447ce3958dede34fc2cb2209701257
Scott Schulthess <sschulthess@zipcar.com>
2017-06-06 11:32:14 -0400

    adding thymerc

--- symlink_all.sh
+++ symlink_all.sh
@@ -1,8 +1,11 @@
 ln -s ~/dotfiles/.ackrc ~/.ackrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
+mkdir ~/.vim/backup
+mkdir ~/.vim/tmp
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.thymerc ~/.thymerc
 
