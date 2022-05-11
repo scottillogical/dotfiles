67668f7cf0fe3ded1a0db3a146254dcf8f6ea88b
Scott Schulthess <sschulthess@zipcar.com>
2022-05-11 13:33:57 -0400

    add safe

--- install.sh
+++ install.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
-set -oux
-
-echo "install vim-plug"
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-echo "installing hombrew"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! command -v brew &> /dev/null
+then
+  echo "[brew] installing"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "[brew] detcted"
+fi
+if ! command -v safe &> /dev/null
+then
+  echo "[safe] installing"
+  curl -fL https://github.com/starkandwayne/safe/releases/latest/download/safe-darwin-amd64 -o ~/bin/safe
+  chmod a+x ~/bin/safe
+else
+  echo "[safe] detected"
+fi
 
 brew install git
 brew install kustomize
 brew install colordiff
 brew install tmux
-brew install neovim
 brew install go
 
 
-
+echo "install vim-plug"
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
