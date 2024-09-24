24edb9968f128fc409772b3e92f154e2a4c49cf8
Scott Schulthess <scottschulthess@gmail.com>
2024-09-24 09:43:10 -0400

    udpate helm installs

--- .zshrc
+++ .zshrc
@@ -27,7 +27,7 @@ export EDITOR=$VISUAL
 
 # add sbin to path
 export PATH="/usr/local/sbin:$PATH"
-export PATH=~/bin:$PATH
+export PATH="~/bin:$PATH"
 export GO_ROOT=/usr/local
 
 #snek
--- install.sh
+++ install.sh
@@ -13,12 +13,14 @@ brew install jenv \
  vault   \
  tmux   \
  go   \
+ helm \
  ag \
  pyenv \
  awscli  \
  starkandwayne/cf/spruce \
  python
 
+brew install --cask fly
 echo "install vim-plug"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
